90c7883c50d85189bf782395c88c277184758759
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2022-01-05 14:06:25 +0100

    promise example

---
+++ tdt/ex01-cb-promise/ex04.js
@@ -0,0 +1,15 @@
+
+const iPromise = new Promise((resolve, reject) => {
+  const number = Math.random()
+  if (number > .5) return resolve(number)
+  else reject('The value is too small')
+})
+
+
+exports.run = () => {
+  console.log(' promise - 01')
+  iPromise
+  .then((number) => console.log(`The number is ${number}`))
+  .catch((error) => {console.error(`An error has occured \n${error}`)})
+}
+
--- tdt/ex01-cb-promise/index.js
+++ tdt/ex01-cb-promise/index.js
@@ -1,8 +1,12 @@
 const ex01 = require('./ex01')
 const ex02 = require('./ex02')
+const ex03 = require('./ex03')
+const ex04 = require('./ex04')
 
 exports.run = () => {
   // ex01.run()
-  ex02.run()
+  // ex02.run()
+  // ex03.run()
+  ex04.run()
 }
 
