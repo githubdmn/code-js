cf43ddbb41c03cf49c317740df74c0c9d4cffba4
githubdmn <dmninbox@gmail.com>
2022-03-08 12:22:45 +0100

    object literal first example

--- tutorial-02/s2.2.object.literals.js
+++ tutorial-02/s2.2.object.literals.js
@@ -2,4 +2,21 @@
 var title = 'ES5\nSection 2 - Video 2 - Object literals\n'
 console.log(title)
 
+var aSquare = {
+	title: 'Square A',
+	side: 4,
+	show: function() {
+		return 'This is Square A with side length 4'
+	}
+}
 
+var bSquare = {
+	title: 'Square B',
+	side: 3,
+	show: function () {
+		return 'This is ' + this.title + ' with side length ' + this.side
+	}
+}
+
+console.log(aSquare.show())
+console.log(bSquare.show())
