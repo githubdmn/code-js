b54d57cc129273690150bf5506392f873fa12f1e
githubdmn <dmninbox@gmail.com>
2022-03-08 13:13:47 +0100

    Constructor Function

--- tutorial-02/index.js
+++ tutorial-02/index.js
@@ -2,4 +2,4 @@
 const title = 'Udemy - Mosh\'s OOP tutorial\n'
 console.log(title)
 
-require('./s2-3-factory')
+require('./s2-4-construnctor')
---
+++ tutorial-02/s2-4-construnctor.js
@@ -0,0 +1,34 @@
+const title = 'ES5\nSection 2 - Constructor'
+console.log(title)
+
+// Factory function ES5
+function createSquare(title, sides) {
+	return {
+		title: title,
+		sides: sides,
+		show: function () {
+			return 'This is a square ' + this.title + ' with side length ' + this.sides
+		}
+	}
+}
+
+var a = createSquare('A', 4)
+console.log(a.show())
+
+// Constructor Function
+function Square(title, sides) {
+	console.log(this)
+	this.title = title
+	this.sides = sides
+	this.show = function() {
+		return 'This is a square ' + this.title + ' with side length ' + this.sides
+	}
+	// Following line is executed automatically
+	// return this
+}
+
+// Implicit instantation
+var b = new Square('B', 5)
+console.log(b.show())
+
+// var c = Square('C', 5) // Object [global] 
