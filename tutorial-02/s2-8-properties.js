2506e8f6db3f75c073daa442ad80b5ae7cc8ae65
githubdmn <dmninbox@gmail.com>
2022-03-18 09:47:36 +0100

    add/remove object properties

--- tutorial-02/s2-8-properties.js
+++ tutorial-02/s2-8-properties.js
@@ -1,3 +1,25 @@
 const title = 'ES5\nSection 2 - Add / Remove properties'
 console.log(title)
 
+function Item(id, title) {
+  this.id = id
+  this.title = title
+  this.show = function() {
+    return "Item with id:" + id + " title:" + title
+  }
+  this.log = function() {
+    console.log(this.show())
+  }
+}
+
+var item = new Item(2, 'Second')
+// item.log()
+// console.log(item['title'])
+var propUserAgent = 'os-browser'
+item[propUserAgent] = 'brave browser'
+console.log(item[propUserAgent])
+
+
+console.log(item['title'])
+delete item['title']
+console.log(item['title'])
