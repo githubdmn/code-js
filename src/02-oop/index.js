bf2d751d9a543227af4f5fbe366bd3d1aa9d5e07
githubdmn <dmninbox@gmail.com>
2023-05-21 21:00:13 +0200

    10-constructor-property running

--- src/02-oop/index.js
+++ src/02-oop/index.js
@@ -1,1 +1,8 @@
 console.log('javascript-object-oriented-programming');
+
+const runFile07 = './07-object-literals';
+const runFile08 = './08-factories';
+const runFile09 = './09-constructors';
+const runFile10 = './10-constructor-property';
+
+require(runFile10);
