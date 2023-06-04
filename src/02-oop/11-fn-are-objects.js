ea20f4643515b6f385ebf91d7c2f590bdf4e1eec
githubdmn <dmninbox@gmail.com>
2023-06-04 20:27:34 +0200

    Functions are objects

--- src/02-oop/11-fn-are-objects.js
+++ src/02-oop/11-fn-are-objects.js
@@ -1,29 +1,31 @@
 console.log('Functions are Objects');
 
-
 function Circle(radius) {
   this.radius = radius;
-  this.draw = function() {
+  this.draw = function () {
     console.log('draw');
-  }
+  };
 }
 
-const anotherCircle = new Function('radius', `
+const anotherCircle = new Function(
+  'radius',
+  `
   this.radius = radius;
   this.draw = function() {
     console.log('draw');
   }
-`);
+`,
+);
 
 const myCircle = new anotherCircle(1);
 
 // Another way of isntatiating an object
 
-// "call" function 
+// "call" function
 // {} refers to this from the object
 // 1 is a parameter corresponding to constructor
 anotherCircle.call({}, 1);
 
 // same as "call" only parameters are passed to constructor
 // as an array
-anothercircle.apply({}, [1])
+anotherCircle.apply({}, [1]);
