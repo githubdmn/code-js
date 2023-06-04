b0353c1688be6f102214e7541f4dcdba7ccad19d
githubdmn <dmninbox@gmail.com>
2023-06-04 20:27:34 +0200

    Functions are objects

--- src/02-oop/index.js
+++ src/02-oop/index.js
@@ -4,5 +4,6 @@ const runFile07 = './07-object-literals';
 const runFile08 = './08-factories';
 const runFile09 = './09-constructors';
 const runFile10 = './10-constructor-property';
+const runFile11 = './11-fn-are-objects';
 
-require(runFile10);
+require(runFile11);
