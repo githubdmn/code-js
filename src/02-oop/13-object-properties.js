ac5b050ae70312134a5260453f7d4e32a4c08285
githubdmn <dmninbox@gmail.com>
2023-06-15 16:25:21 +0200

    object properties formatted

--- src/02-oop/13-object-properties.js
+++ src/02-oop/13-object-properties.js
@@ -1,12 +1,10 @@
 console.log('Object properties');
 
-
 function Circle(radius) {
-
   this.radius = radius;
-  this.draw = function() {
+  this.draw = function () {
     console.log('draw');
-  }
+  };
 }
 
 const circle = new Circle(10);
@@ -14,7 +12,7 @@ const circle = new Circle(10);
 // console.log(Object.getOwnPropertyNames(circle));
 console.log(Object.keys(circle));
 
-circle.location = { x:1 };
+circle.location = { x: 1 };
 // alternative notation
 // circle['location'] = { x:1 };
 // used when field has a special character in its name
