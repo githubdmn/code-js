9e9f0cc26bf763510adab378167f7ced2b1c5e27
githubdmn <dmninbox@gmail.com>
2023-06-20 09:39:28 +0200

    gettres and setters - Object.defineProperty

--- src/02-oop/index.js
+++ src/02-oop/index.js
@@ -8,5 +8,6 @@ const runFile11 = './11-fn-are-objects';
 const runFile13 = './13-object-properties';
 const runFile14 = './14-enumerating-properties';
 const runFile16 = './16-private-properties-methods';
+const runFile17 = './17-getters-setters';
 
-require(runFile16);
+require(runFile17);
