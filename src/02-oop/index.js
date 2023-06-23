6d449bfcc680add84f15d14083bf340bf91e50a6
githubdmn <dmninbox@gmail.com>
2023-06-23 18:26:31 +0200

    cheatsheet

--- src/02-oop/index.js
+++ src/02-oop/index.js
@@ -9,5 +9,10 @@ const runFile13 = './13-object-properties';
 const runFile14 = './14-enumerating-properties';
 const runFile16 = './16-private-properties-methods';
 const runFile17 = './17-getters-setters';
+const runFile21 = './21-inheritance';
+const runFile24 = './24-property-descriptors';
+const runFile25 = './25-constructor-prototype';
+const runFile26 = './26-prototype-vs-inheritance-members';
+const runFile27 = './27-iterating-members';
 
-require(runFile17);
+require(runFile27);
