452b1e0820f78f641d4ada61f8ad9c14cf9ae825
githubdmn <dmninbox@gmail.com>
2023-06-30 18:10:26 +0200

    Object.getPrototypeOf

--- src/02-oop/25-constructor-prototype.js
+++ src/02-oop/25-constructor-prototype.js
@@ -1,7 +1,7 @@
 console.log('\tConstructor prototype\n');
 
 // Object.prototype.constructor
-// The constructor data property of an Object instance returns 
+// The constructor data property of an Object instance returns
 // a reference to the constructor function that created the instance object.
 // Note that the value of this property is a reference to the function itself,
 // not a string containing the function's name.
