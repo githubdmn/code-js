7a2dcc64867572dc7189e735f3b5f4f77242c98d
githubdmn <dmninbox@gmail.com>
2023-07-12 17:14:42 +0200

    balanced brackets

--- src/misc/index.js
+++ src/misc/index.js
@@ -1,6 +1,7 @@
 console.log('\nmisc files\n');
 
+const interview00 = './interview-00';
 const interview01 = './interview-01';
 const interview02 = './interview-02';
 
-require(interview02);
+require(interview00);
