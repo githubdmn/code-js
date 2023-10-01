ddfd7fdb2ecd47726b577d6c714e2b9d662215cc
githubdmn <dmninbox@gmail.com>
2023-10-01 17:17:19 +0200

    alg

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('index js addition');
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 require('./02-oop');
 =======
 require('./misc');
@@ -12,3 +13,9 @@ require('./04-algorithms');
 require('./05-algorithms');
 require('./06-algorithms');
 >>>>>>> d231078 (algorithms setup)
+=======
+//require('./03-algorithms');
+//require('./04-algorithms');
+require('./05-algorithms');
+//require('./06-algorithms');
+>>>>>>> 924725e (Master the Coding Interview Data Structures + Algorithms)
